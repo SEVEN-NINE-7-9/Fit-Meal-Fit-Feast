6ba6a09f8e97eb4356644a77963a6f6b0867082e
SEVEN-NINE-7-9 <samarthbg13097@gmail.com>
2026-05-18 12:01:15 +0530

    6th commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2cC5iLJfXJ-M_M1AF00cogr_PrdgbBLE",
-  authDomain: "fitmeal-1.firebaseapp.com",
-  projectId: "fitmeal-1",
-  storageBucket: "fitmeal-1.firebasestorage.app",
-  messagingSenderId: "890273092385",
-  appId: "1:890273092385:web:8e7b5e7918606367d5a460"
+ 
 };
 
 // Initialize Firebase
